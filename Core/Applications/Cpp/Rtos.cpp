9d187f555bb56705b1df73f4cf84e48f97634198
dys <2771329963@qq.com>
2025-04-19 06:12:59 +0800

    除了矫正方向以及位控，其余均实现

---
+++ Core/Applications/Cpp/Rtos.cpp
@@ -0,0 +1,77 @@
+#include "Rtos.h"
+#include "cmsis_os2.h"
+#include "fdcan.h"
+#include "vofa.h"
+#include "Cpp/Upper_Visual.h"
+Upper_data_receive Vdata_Rx;
+Upper_data_send Vdata_Tx;
+#ifdef __cplusplus
+extern "C" {
+#endif
+    /*---------------------------- C Scope Start---------------------------*/
+
+
+void Vofa_to_Motor()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        if (VofaButton[i] == 1)
+        {
+            set_zero_motor(&hfdcan1,i);
+            set_zero_motor(&hfdcan2,i);
+            set_zero_motor(&hfdcan3,i);
+        }
+    }
+    for (int i = 8; i < 12; i++)
+    {
+        if (VofaKey[i - 8] == 1)
+        {
+            set_zero_motor(&hfdcan1,i);
+            set_zero_motor(&hfdcan2,i);
+            set_zero_motor(&hfdcan3,i);
+        }
+    }
+}
+void VofaReceiveTask(void *argument)
+    {
+        /* USER CODE BEGIN VofaReceiveTask */
+        /* Infinite loop */
+        for(;;)
+        {
+            Vofa_to_Motor();
+            Vofa_Transmit(&huart1,51);
+            osDelay(1);
+        }
+        /* USER CODE END VofaReceiveTask */
+    }
+
+    /* USER CODE BEGIN Header_UART_TX_task */
+    /**
+    * @brief Function implementing the UART_TX thread.
+    * @param argument: Not used
+    * @retval None
+    */
+    /* USER CODE END Header_UART_TX_task */
+void UART_TX_task(void *argument)
+{
+    /* USER CODE BEGIN UART_TX_task */
+
+    /* Infinite loop */
+    for(;;)
+    {
+        Vofa_Transmit(&huart1,51);
+        Vdata_Tx.All_Data_get();
+        Vdata_Tx.All_Data_send(&huart7);
+        osDelay(1);
+    }
+    /* USER CODE END UART_TX_task */
+}
+    /*---------------------------- C Scope End---------------------------*/
+#ifdef __cplusplus
+}
+#endif
+
+
+
+
+
---
+++ Core/Applications/Cpp/Rtos.h
@@ -0,0 +1,26 @@
+//
+// Created by dys on 25-4-18.
+//
+
+#ifndef RTOS_H
+#define RTOS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    /*---------------------------- C Scope Start---------------------------*/
+
+
+void Vofa_to_Motor();
+
+
+
+
+
+    /*---------------------------- C Scope End---------------------------*/
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif //RTOS_H
